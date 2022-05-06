c4ea8f606a990364d31e062f856dd675bd1209bd
kalen510 <kalen.wiley@gmail.com>
2022-05-05 22:28:37 -0400

    high scores js

---
+++ assets/js/highscores.js
@@ -0,0 +1,25 @@
+var highScore = document.querySelector("#highScore");
+var clearScores = document.querySelector("#clear");
+var goBack = document.querySelector("#goBack");
+
+// get all scores from localStorage
+var scores = localStorage.getItem("scores");
+scores = JSON.parse(scores);
+
+if (scores) {
+    for (var i = 0; i < scores.length; i++) {
+        var createLi = document.createElement("li");
+        createLi.textContent = scores[i].initials + " " + scores[i].score;
+        highScore.appendChild(createLi);
+    }
+}
+
+// clear stored scores from localStorage
+clearScores.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+
+goBack.addEventListener("click", function () {
+    window.location.replace("./index.html");
+});
